f996b20733195f62164edc0fe1880ed39e57f92e
Samuel Thapa Magar <samuel@vrittechnologies.com>
2026-01-30 16:58:17 +0545

    sign up user

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:taski/firebase_options.dart';
 import 'package:taski/providers/task_provider.dart';
 import 'package:taski/screens/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
